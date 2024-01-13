658c91c21560b762c8ed76706c81f2fb07030cf9
Hiep Vo Van <hiepvo148@gmail.com>
2024-01-13 19:28:33 +0700

    update config

--- config/config.go
+++ config/config.go
@@ -5,8 +5,8 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-func InitConfig(cfg interface{}) error {
-	err := cleanenv.ReadConfig("config.yml", cfg)
+func InitConfig(cfgPath string, cfg interface{}) error {
+	err := cleanenv.ReadConfig(cfgPath, cfg)
 	if err != nil {
 		fmt.Printf("error load config : %s", err.Error())
 		return err
